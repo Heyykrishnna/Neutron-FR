5e236a1d6bdf9e1ca07604c9f9bf933e6ed38ce6
Yatharth <khandelwalyatharth39@gmail.com>
2026-03-22 20:06:48 +0530

    Fonts done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Audiowide, Space_Grotesk} from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const audiowideSans = Audiowide({
+  variable: "--font-audiowide",
   subsets: ["latin"],
+  weight: '400',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${audiowideSans.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
